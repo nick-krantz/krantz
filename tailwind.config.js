dfeaf18c4d1f9fbe3f70535df9028de2320764d9
Nick Krantz <krantni@gmail.com>
2022-01-08 20:38:52 -0600

    mobile grid list improvements

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,7 @@ module.exports = {
       },
       gridTemplateColumns: {
         'recipe-grid': 'repeat(auto-fit, minmax(240px, 1fr))',
+        'recipe-grid-sm': 'repeat(auto-fit, minmax(140px, 1fr))',
       },
     },
   },
