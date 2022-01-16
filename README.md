beaf1f64f5d36ea79fab74679e7666284fa68c73
Nick Krantz <krantni@gmail.com>
2022-01-16 15:09:16 -0600

    adding active state of menu nav item

--- README.md
+++ README.md
@@ -31,5 +31,5 @@ Icons: [react-icons/fi](https://react-icons.github.io/react-icons/icons?name=fi)
 - [ ] add edit for recipes
 - [x] change authenticated param order, guardedRoute should be before the callback 
 - [ ] dynamic theme-color meta tag based on user preference
-- [ ] add active state to menu nav items
+- [x] add active state to menu nav items
 - [ ] initial focus on menu close button
--- app/components/menu/nav-item.tsx
+++ app/components/menu/nav-item.tsx
@@ -5,7 +5,19 @@ import { Icon } from '../icon'
 export const NavItem: React.FC<{ to: string; icon: IconType }> = ({ children, to, icon }) => {
   return (
     <li className="p-4 text-lg">
-      <NavLink to={to} className="flex items-center gap-6 p-2 rounded-md">
+      <NavLink
+        to={to}
+        className="flex items-center gap-6 p-2 rounded-md"
+        style={({ isActive }) =>
+          isActive
+            ? {
+                borderWidth: '2px',
+                borderStyle: 'solid',
+                borderColor: 'currentcolor',
+              }
+            : {}
+        }
+      >
         <Icon Icon={icon} />
         {children}
       </NavLink>
