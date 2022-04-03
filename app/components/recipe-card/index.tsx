3c9b49664df70084fceccf75b04de64f2e102cca
Nick Krantz <krantni@gmail.com>
2022-04-03 10:04:59 -0500

    fixing react error for javascript:void()

--- app/components/recipe-card/index.tsx
+++ app/components/recipe-card/index.tsx
@@ -7,6 +7,9 @@ type Props = {
   preview?: boolean
 }
 
+const actionClass =
+  'inline-flex items-center p-3 rounded-md text-gray-800 bg-gray-300 dark:text-gray-200 dark:bg-slate-700'
+
 export const RecipeCard: React.FC<Props> = ({ recipe: { name, image, url, id }, preview }) => {
   const navigation = useNavigate()
 
@@ -39,30 +42,21 @@ export const RecipeCard: React.FC<Props> = ({ recipe: { name, image, url, id }, 
       <div className="flex flex-col items-start h-full flex-1 p-4 gap-5">
         <p className="font-semibold text-left flex-1 overflow-hidden text-ellipsis line-clamp-3">{name}</p>
         <div className="flex w-full gap-3">
-          <a
-            className="inline-flex items-center p-3 rounded-md text-gray-800 bg-gray-300 dark:text-gray-200 dark:bg-slate-700"
-            href={!preview ? url : 'javascript:void(0)'}
-            style={preview ? { pointerEvents: 'none', opacity: '.5' } : {}}
-            aria-label={preview ? 'Open recipe (disabled)' : 'Open Recipe'}
-          >
-            <FiExternalLink />
-          </a>
+          {preview ? (
+            <div className={actionClass}>
+              <FiExternalLink />
+            </div>
+          ) : (
+            <a className={actionClass} href={url} aria-label="Open Recipe">
+              <FiExternalLink />
+            </a>
+          )}
 
-          <button
-            className="inline-flex items-center p-3 rounded-md text-gray-800 bg-gray-300 dark:text-gray-200 dark:bg-slate-700  disabled:opacity-50"
-            aria-label={`Edit ${name}`}
-            onClick={edit}
-            disabled={preview}
-          >
+          <button className={actionClass} aria-label={`Edit ${name}`} onClick={edit} disabled={preview}>
             <FiEdit />
           </button>
 
-          <button
-            className="inline-flex items-center p-3 rounded-md text-gray-800 bg-gray-300 dark:text-gray-200 dark:bg-slate-700  disabled:opacity-50"
-            aria-label={`Share ${name}`}
-            onClick={share}
-            disabled={preview}
-          >
+          <button className={actionClass} aria-label={`Share ${name}`} onClick={share} disabled={preview}>
             <FiShare2 />
           </button>
         </div>
