9297da850144c032ad5c41c51d701afbb7a9549c
Nick Krantz <krantni@gmail.com>
2022-06-12 09:42:30 -0500

    move all pages under __app layout route

---
+++ app/routes/__app.tsx
@@ -0,0 +1,20 @@
+import { LoaderFunction, Outlet, useLoaderData } from 'remix'
+import { Header } from '~/components/header'
+import { authenticated } from '~/utils/supabase/authenticated'
+
+export const loader: LoaderFunction = ({ request }) => {
+  return authenticated(request, false, ({ authorized }) => {
+    return Promise.resolve({ authorized })
+  })
+}
+
+export default function Index() {
+  const { authorized } = useLoaderData<{ authorized: boolean }>()
+
+  return (
+    <div className="flex flex-col h-full">
+      <Header authorized={authorized} />
+      <Outlet />
+    </div>
+  )
+}
