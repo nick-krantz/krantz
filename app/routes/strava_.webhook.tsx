a66188d708c0b2edca950ab52793d913631062b8
Nick Krantz <nick@krantz.app>
2024-03-06 19:12:12 -0600

    add hook to update activity

--- app/routes/strava_.webhook.tsx
+++ app/routes/strava_.webhook.tsx
@@ -94,6 +94,83 @@ const saveWorkoutToDB = async (workoutId: string) => {
   }
 };
 
+
+const updateWorkoutToDB = async (workoutId: string) => {
+  try {
+    // Get tokens from Supabase
+    const { data, error } = await supabase
+      .from("strava")
+      .select()
+      .eq("id", "1")
+      .limit(1)
+      .single();
+
+    if (!data || error) {
+      throw new Error(`No strava data found ${error.message}`);
+    }
+
+    // Set up strava client with attributes
+    strava.config({
+      client_id: process.env.STRAVA_CLIENT_ID!,
+      client_secret: process.env.STRAVA_CLIENT_SECRET!,
+      redirect_uri: process.env.STRAVA_REDIRECT_URI!,
+      access_token: data.access_token,
+    });
+
+    // Refresh the strava tokens
+    const refreshResponse = await strava.oauth.refreshToken(data.refresh_token);
+
+    // Update the tokens in the supabase db
+    await supabase
+      .from("strava")
+      .update({
+        access_token: refreshResponse.access_token,
+        refresh_token: refreshResponse.refresh_token,
+        expires_at: refreshResponse.expires_at,
+      })
+      .eq("id", "1");
+
+    const activityResponse = await strava.activities.get({
+      id: workoutId,
+      includeAllEfforts: true,
+      access_token: refreshResponse.access_token,
+    });
+
+    const workout = {
+      average_speed: activityResponse.average_speed,
+      distance: activityResponse.distance!,
+      elapsed_time: activityResponse.elapsed_time!,
+      strava_id: activityResponse.id,
+      moving_time: activityResponse.moving_time!,
+      title: activityResponse.name,
+      // start_date is a timestamp string
+      start_date: activityResponse.start_date as unknown as string,
+      description: activityResponse.description!,
+      sport_type: activityResponse.sport_type,
+      // strava types are a little off here, splits_standard does exist on the response
+      // but the types don't match up
+      splits_standard: (activityResponse as any).splits_standard,
+    };
+
+    const response = await supabase
+      .from("workouts")
+      .update({
+        ...workout,
+      })
+      .eq('strava_id', workout.strava_id);
+
+    if (response.error) {
+      console.error(response.error);
+      return;
+    }
+
+    console.log(`Updated Strava Activity: ${response.data?.[0].id}`);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+
 export const action: ActionFunction = async ({ request }) => {
   console.log("Strava webhook action received!", request.url);
 
@@ -101,6 +178,10 @@ export const action: ActionFunction = async ({ request }) => {
   console.log({ body });
   if (body.object_type === "activity" && body.aspect_type === "create") {
     await saveWorkoutToDB(body.object_id);
+  }
+
+  if (body.object_type === "activity" && body.aspect_type === "update") {
+    await updateWorkoutToDB(body.object_id);
   }
 
   return new Response("Event Received!", { status: 200 });
