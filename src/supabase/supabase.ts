07586dc01ccf06d7641c1c52de130b4a5decec96
Nick Krantz <nick@krantz.app>
2025-04-09 17:48:01 -0500

    cleanup

--- src/supabase/supabase.ts
+++ src/supabase/supabase.ts
@@ -2,13 +2,6 @@ import { createClient } from "@supabase/supabase-js";
 import type { Database } from "./types/Database";
 import { SUPABASE_URL, SUPABASE_ANON_KEY } from "astro:env/server";
 
-console.log(
-  "Supabase URL: ",
-  SUPABASE_URL,
-  "Supabase Anon Key: ",
-  SUPABASE_ANON_KEY
-);
-
 // Create supabase client to be used throughout the application
 export const supabase = createClient<Database>(
   SUPABASE_URL,
