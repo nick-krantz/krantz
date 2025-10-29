c0d0a4b45ac1412cefc98d317fb67dda4fd5c1ba
Nick Krantz <nick@krantz.app>
2025-10-28 21:04:24 -0500

    remove old app folder from remix app

--- app/components/backdrop/index.tsx
+++
@@ -1,3 +0,0 @@
-export const BackDrop: React.FC = () => (
-	<div className="fixed h-screen w-screen top-0 left-0 z-10 bg-black opacity-40 dark:opacity-60" />
-);
--- app/components/icon/index.tsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { IconType } from "react-icons";
-
-export const Icon: React.FC<{ Icon: IconType | React.FC }> = ({ Icon }) => (
-	<Icon className="inline-block w-8 h-8" />
-);
